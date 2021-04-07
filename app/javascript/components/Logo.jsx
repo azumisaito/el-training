12c3f4b20cb269bbd6feaacf76dc0a389b94264c
sugiii8 <t.sugie.1000@gmail.com>
2021-04-07 23:37:13 +0900

    コンポーネントサンプル:ロゴコンポーネント

---
+++ app/javascript/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../images/mofmof.png'
+
+export const Logo = (props) => {
+  const width = (props.width) ? props.width : 200
+  return (
+    <div>
+      <img src={logo} alt="mofmof" width={width} />
+    </div>
+  )
+}
