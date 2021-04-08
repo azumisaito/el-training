59066519be64aa93d1854db4615dca29ef21dac3
Azumi S <seinoazumi@gmail.com>
2021-04-08 18:17:12 +0900

    タスク一覧を表示

--- app/javascript/pages/Labels.jsx
+++ app/javascript/pages/Labels.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '~/components/Button'
 import { PageTitle } from '~/components/PageTitle'
 import { TextField } from '~/components/TextField'
 
-const buttonLabel = "ラベルを作成"
+import axios from 'axios'
 
 export const Labels = (props) => {
+  const [labels, setLabels] = useState([]),
+        buttonLabel = "ラベルを作成"
+
+  React.useEffect(async () => {
+    const response = await axios.get('/api/labels');
+    setLabels(response.data)
+  }, [])
+
   return (
     <div>
       <PageTitle />
       <TextField />
       <Button label={buttonLabel} />
       <p>こんにちは。{props.name}さん</p>
+      <ul>
+      {labels.map(label => (
+        <li key={label.id}>
+          {label.name}
+        </li>
+      ))}
+    </ul>
     </div>
   )
 }
