8c2c03f613e2129a1f721089da154573a1e5a3b9
Azumi S <seinoazumi@gmail.com>
2021-04-09 13:03:02 +0900

    Rails側の設定を追記

--- app/controllers/api/labels_controller.rb
+++ app/controllers/api/labels_controller.rb
@@ -3,4 +3,19 @@ class Api::LabelsController < ApplicationController
     @labels = current_user.labels
     render json: @labels
   end
+
+  def create
+    label = current_user.labels.build(label_params)
+    if label.save
+      render json: label
+    else
+      lender json: label.errors
+    end
+  end
+
+  private
+
+  def label_params
+    params.require(:label).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   end
 
   namespace :api do
-    resources :labels, only: %i[index]
+    resources :labels, only: %i[index create]
   end
 
   get '*path', to: 'application#routing_error'
