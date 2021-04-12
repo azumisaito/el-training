4a32705ca17f339c2edf6cb101e02afe939708f6
Azumi S <seinoazumi@gmail.com>
2021-04-12 10:47:56 +0900

    タスク一覧表示画面の作成

--- app/javascript/pages/Tasks.jsx
+++ app/javascript/pages/Tasks.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
+import { LabelCreateForm } from '~/components/LabelCreateForm'
+import { Labels } from './Labels'
 
 export const Tasks = () => {
+  const [tasks, setTasks] = useState([])
+
+  React.useEffect(async () =>{
+    const response = await axios.get('/api/tasks');
+    setTasks(response.data)
+  }, [])
+
   return (
     <div>
-      ほげ
+      <h1>タスク一覧</h1>
+      <LabelCreateForm />
+      <table>
+        <thead>
+          <tr>
+            <th>タスク名</th>
+            <th>終了期限</th>
+            <th>ステータス</th>
+            <th>優先順位</th>
+            <th>ラベル</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map(task => (
+            <tr key={task.id}>
+              <td>
+                <a href="#">{task.name}</a>
+              </td>
+              <td>
+                {task.end_date}
+              </td>
+              <td>
+                {task.status} {/* TODO: enumを日本語にしたい */}
+              </td>
+              <td>
+                {task.priority}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
