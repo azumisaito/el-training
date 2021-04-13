52e36655992c3366dee4b1b85251ec2b086324b1
Azumi S <seinoazumi@gmail.com>
2021-04-13 18:29:59 +0900

    フォームの見た目の作成

--- app/javascript/components/TaskCreateForm.jsx
+++ app/javascript/components/TaskCreateForm.jsx
@@ -4,7 +4,23 @@ export const TaskCreateForm = () => {
 
   return(
     <div>
-      <p>ここにフォームが入ります</p>
+      <form>
+        <label>
+          <p>タスク名</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>詳細</p>
+          <textarea rows="4" cols="40"></textarea>
+        </label>
+        <label>
+          <p>終了期限</p>
+          <input type="date" />
+        </label>
+        <p>
+          <input type="submit" value="登録する" />
+        </p>
+      </form>
     </div>
   )
 }
