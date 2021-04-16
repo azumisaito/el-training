413d6a169d8427106a9afff19d7e1f2ccf7ef828
Azumi S <seinoazumi@gmail.com>
2021-04-16 18:43:22 +0900

    タスク名、詳細、終了期限が登録できるようにする

--- app/javascript/components/TaskCreateForm.jsx
+++ app/javascript/components/TaskCreateForm.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 export const TaskCreateForm = () => {
-
   const token = document.querySelector('[name=csrf-token]').content
   axios.defaults.headers.common['X-CSRF-TOKEN'] = token
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [endDate, setEndDate] = useState("");
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
     axios.post("/api/tasks", {
       task: {
-        name: "test",
-        description: "テストの説明",
-        end_date: "",
+        name: name,
+        description: description,
+        end_date: endDate,
         status: 0,
         priority: 0,
         user_id: 1
@@ -20,20 +24,36 @@ export const TaskCreateForm = () => {
     })
   }
 
+  const handleChange = (event) => {
+    switch (event.target.name) {
+      case 'name':
+        setName(event.target.value);
+        break;
+      case 'description':
+        setDescription(event.target.value);
+        break;
+      case 'endDate':
+        setEndDate(event.target.value);
+        break;
+      default:
+        console.log('key not found');
+    }
+  };
+
   return(
     <div>
       <form onSubmit={handleSubmit}>
         <label>
           <p>タスク名</p>
-          <input type="text" />
+          <input type="text" name="name" value={name} onChange={handleChange} />
         </label>
         <label>
           <p>詳細</p>
-          <textarea rows="4" cols="40"></textarea>
+          <textarea rows="4" cols="40" name="description" value={description} onChange={handleChange} ></textarea>
         </label>
         <label>
           <p>終了期限</p>
-          <input type="date" />
+          <input type="date" name="endDate" value={endDate} onChange={handleChange} />
         </label>
         <p>
           <input type="submit" value="登録する" />
